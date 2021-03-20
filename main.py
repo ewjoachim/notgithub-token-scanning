3a65406c1e6d969f92c2cb633980b878e3c60b23
Joachim Jablon <ewjoachim@gmail.com>
2021-03-20 22:04:55 +0100

    v2

--- main.py
+++ main.py
@@ -15,9 +15,23 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-keys: list = []
 latest_response = None
-errors: list = []
+state: dict = {
+    "payload": {
+        "value": json.dumps(
+            [{"type": "token", "token": "some-token", "url": "https://example.com"}]
+        ),
+        "error": False,
+    },
+    "url": {
+        "value": os.environ.get(
+            "NOTGITHUB_DEFAULT_URL", "http://somewhere/_/github/disclose"
+        ),
+        "error": False,
+    },
+    "response": None,
+    "keys": [],
+}
 
 
 def create_keypair():
@@ -27,8 +41,8 @@ def create_keypair():
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo,
     ).decode()
-    is_current = not any(k["is_current"] for k in keys)
-    keys.append(
+    is_current = not any(k["is_current"] for k in state["keys"])
+    state["keys"].append(
         {
             "id": hashlib.sha256(f"{id(private_key)}".encode()).hexdigest(),
             "private_key": private_key,
@@ -38,17 +52,22 @@ def create_keypair():
     )
 
 
-def sign(key, payload: list):
+def sign(key, payload: str):
     return base64.b64encode(
         key["private_key"].sign(
-            json.dumps(payload).encode("utf-8"),
+            payload.encode("utf-8"),
             signature_algorithm=ec.ECDSA(algorithm=SHA256()),
         )
     ).decode()
 
 
 async def disclose(url, payload):
-    key = next(iter(k for k in keys if k["is_current"]))
+    key = next(iter(k for k in state["keys"] if k["is_current"]), None)
+    if not key:
+        return {
+            "status_code": "-",
+            "text": "No current key. Please set a key as current.",
+        }
     signature = sign(key=key, payload=payload)
 
     try:
@@ -58,8 +77,9 @@ async def disclose(url, payload):
                 headers={
                     "GITHUB-PUBLIC-KEY-IDENTIFIER": key["id"],
                     "GITHUB-PUBLIC-KEY-SIGNATURE": signature,
+                    "Content-Type": "application/json",
                 },
-                json=payload,
+                data=payload,
             )
     except httpx.TransportError as exc:
         return {"status_code": "-", "text": str(exc)}
@@ -68,35 +88,31 @@ async def disclose(url, payload):
 
 @app.get("/", response_class=responses.HTMLResponse)
 async def home_view(request: Request):
-    context: dict = {}
-    global latest_response
-    if latest_response:
-        context["response"] = latest_response
-        latest_response = None
-    if errors:
-        context["errors"] = errors[:]
-        print(context["errors"])
-        errors.clear()
+    if not state["keys"]:
+        create_keypair()
 
-    context["default_url"] = os.environ.get(
-        "NOTGITHUB_DEFAULT_URL", "http://somewhere/_/github/disclose"
-    )
-
-    return templates.TemplateResponse(
-        "home.html.j2", {"request": request, "keys": keys, **context}
-    )
+    return templates.TemplateResponse("home.html.j2", {"request": request, **state})
 
 
 @app.post("/disclose")
 async def disclose_view(url: str = Form(""), payload: str = Form("")):
-    global latest_response, errors
-    if not url:
-        errors.append("url")
-    if not payload:
-        errors.append("payload")
 
-    if not errors and keys:
-        latest_response = await disclose(url=url, payload=payload)
+    state["url"]["value"] = url
+    state["payload"]["value"] = payload
+
+    errors = False
+
+    # Not checking anything on payload to leave all the room for testing edcases
+    # (missing or malformed body)
+    if not payload:
+        state["payload"]["error"] = "Missing url"
+        errors = True
+
+    if not errors:
+        state["response"] = await disclose(url=url, payload=payload)
+
+    state["url"]["error"] = False
+    state["payload"]["error"] = False
 
     return responses.RedirectResponse("/", status_code=302)
 
@@ -112,6 +128,7 @@ async def edit_key_view(
     i: int,
     action: str = Form(...),
 ):
+    keys = state["keys"]
     redir = responses.RedirectResponse("/", status_code=302)
     if not 0 <= i < len(keys):
         return redir
@@ -138,6 +155,6 @@ def meta_list_keys():
                 "key": key["public_key"],
                 "is_current": key["is_current"],
             }
-            for key in keys
+            for key in state["keys"]
         ]
     }
